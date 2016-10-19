a852b5a8fe040f917c1f3fb76291b35709cefc14
Aji Slater <aji@Christophers-MacBook-Pro-2.local>
2016-10-19 18:37:41 -0500

    Installs basic phaser js files

---
+++ js/game.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  var width = window.innerWidth,
+      height = window.innerHeight,
+      game = new Phaser.Game(width, height, Phaser.AUTO, 'game', null, true, false);
+
+  game.state.add('boot', bootState);
+  game.state.add('load', loadState);
+  game.state.add('play', playState);
+  game.state.start('boot');
+};
